8776bd9aee14e2833c8cff33e33025df9bb6388c
Kim Minsoo <minsoo@local>
2026-01-01 16:36:35 +0900

    fix(build): use nativewind/babel as preset (not plugin)

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel"],
+    // NativeWind v4: use as a preset (it returns { plugins: [...] }),
+    // not as a Babel plugin entry (which would error in CI/EAS).
+    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
+    plugins: [],
   };
 };
 
